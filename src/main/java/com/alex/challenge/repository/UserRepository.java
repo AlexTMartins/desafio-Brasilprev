1639fee6ee654f53dd0f8584d657d9b28b5db8eb
Alex <Alex@KAHENA.mshome.net>
2020-12-14 21:48:41 -0200

    cleanig project to start over again.

--- src/main/java/com/alex/challenge/repository/UserRepository.java
+++ src/main/java/com/alex/challenge/repository/UserRepository.java
@@ -1,11 +1,10 @@
 package com.alex.challenge.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.alex.challenge.model.v1.User;
 
-@RepositoryRestResource(path = "users")
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findUserByNameAndDocument(String name, String doc);
