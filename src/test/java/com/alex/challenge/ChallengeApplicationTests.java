5450b86e32a5856b4f935512fae1a00e6f9d28e8
unknown <Alex@KAHENA.admoto.moto.com.br>
2020-12-13 22:32:24 -0300

    init brasilprev challenge

---
+++ src/test/java/com/alex/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
