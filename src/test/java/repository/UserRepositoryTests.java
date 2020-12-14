b89d0c8af7e6ae62ff1e0401fdf6d1806cb95a98
Alex <Alex@KAHENA.mshome.net>
2020-12-14 03:36:24 -0200

    add user repository main tests

---
+++ src/test/java/repository/UserRepositoryTests.java
@@ -0,0 +1,53 @@
+package repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.alex.challenge.model.User;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void insertUser() {
+    	
+        User user = User.builder().id(1L).name("Alex").document("12345678900").build();
+        userRepository.save(user);
+        Integer countUser = userRepository.findAll().size();
+        assertEquals(1, countUser);
+    }
+
+    @Test
+    public void checkUserSavedWithDocument() {
+    	User user = User.builder().id(1L).name("Alex").document("12345678900").build();
+        userRepository.save(user);
+        Integer countUser = userRepository.findAll().size();
+        assertEquals(1, countUser);
+        User user1 = userRepository.findUserByNameAndDocument("name", "12345678900");
+
+        assertNotNull(user1);
+        assertEquals(user, user1);
+    }
+
+    @Test
+    public void checkUserSavedWithDocumentPassingOtherDocumentShouldReturnNull() {
+    	User user = User.builder().id(1L).name("Alex").document("12345678900").build();
+        userRepository.save(user);
+        Integer countUser = userRepository.findAll().size();
+        assertEquals(1, countUser);
+        User user1 = userRepository.findUserByNameAndDocument("Alex", "12345678901");
+
+        assertNull(user1);
+    }
+}
