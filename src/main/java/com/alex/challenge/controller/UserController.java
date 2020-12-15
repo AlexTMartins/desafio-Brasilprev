e1405e1de495357f6eba76329b043801fc584f01
Alex <Alex@KAHENA.mshome.net>
2020-12-15 02:11:57 -0200

    Add CRUD methods to controller and removing useless service

--- src/main/java/com/alex/challenge/controller/UserController.java
+++ src/main/java/com/alex/challenge/controller/UserController.java
@@ -1,31 +1,32 @@
 package com.alex.challenge.controller;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.challenge.model.v1.User;
-import com.alex.challenge.service.UserService;
-
-import lombok.Setter;
+import com.alex.challenge.repository.UserRepository;
 
 @RestController
-@Setter
 public class UserController {
 
 	@Autowired
-	private UserService userService;
+	private UserRepository userRepository;
 	
-	@RequestMapping(value = "/v1/users/{id}", method = RequestMethod.GET)
-	public ResponseEntity<User> GetById(@PathVariable(value = "id") long id) {
+	@GetMapping("/v1/users/{id}")
+	public ResponseEntity<User> getById(@PathVariable(value = "id") Long id) {
 
-		Optional<User> optUser = userService.getById(id);
+		Optional<User> optUser = userRepository.findById(id);
 		
 		if (optUser.isPresent()) {
 			return new ResponseEntity<User>(optUser.get(), HttpStatus.OK);
@@ -33,4 +34,40 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
+	
+	@PostMapping("/v1/users/")
+	public User post(@RequestBody User user) {
+	    return userRepository.save(user);
+	}
+
+    @PutMapping("/v1/users/{id}")
+    public ResponseEntity<User> put(@PathVariable(value = "id") Long id, @RequestBody User newUser)
+    {
+        Optional<User> oldUser = userRepository.findById(id);
+        if(oldUser.isPresent()){
+            User user = oldUser.get();
+            user.setName(newUser.getName());
+            user.setDocument(newUser.getDocument());
+            user.setAddress(newUser.getAddress());
+            
+            userRepository.save(user);
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        }
+        else {
+        	return new ResponseEntity<>(HttpStatus.NOT_FOUND);        	
+        }
+    }
+
+
+    @DeleteMapping("/v1/users/{id}")
+    public ResponseEntity<Object> delete(@PathVariable(value = "id") Long id){
+    	Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+        	userRepository.delete(user.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else {        	
+        	return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
