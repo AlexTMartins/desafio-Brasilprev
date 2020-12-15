51625dc7dac18f1bd07671aee36285de2b2e7a5a
Alex <Alex@KAHENA.mshome.net>
2020-12-15 01:34:17 -0200

    add GET user endpoint

--- src/main/java/com/alex/challenge/controller/UserController.java
+++ src/main/java/com/alex/challenge/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.alex.challenge.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,18 +11,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.challenge.model.v1.User;
+import com.alex.challenge.service.UserService;
+
+import lombok.Setter;
 
 @RestController
+@Setter
 public class UserController {
 
-	   @RequestMapping(value = "/v1/users/{id}", method = RequestMethod.GET)
-	    public ResponseEntity<User> GetById(@PathVariable(value = "id") long id)
-	    {
-		   User user = User.builder().name("Alex").document("12332112300").build();
-		   if(true) {			   
-			   return new ResponseEntity<User>(user , HttpStatus.OK);
-		   }else {
-			   return new ResponseEntity<>(HttpStatus.NOT_FOUND);			   
-		   }
-	    }
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value = "/v1/users/{id}", method = RequestMethod.GET)
+	public ResponseEntity<User> GetById(@PathVariable(value = "id") long id) {
+
+		Optional<User> optUser = userService.getById(id);
+		
+		if (optUser.isPresent()) {
+			return new ResponseEntity<User>(optUser.get(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
